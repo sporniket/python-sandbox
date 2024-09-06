4c88be0eb69ccae84ee834faf95bcbe332963ef6
David SPORN <sporniket.studio@gmail.com>
2024-09-06 08:18:53 +0200

    refactoring -- remove static_parser.Range

--- py-isa-m68k/src/isa_m68k/assembly/static_parser.py
+++ py-isa-m68k/src/isa_m68k/assembly/static_parser.py
@@ -27,65 +27,6 @@ TypeOfNode = Enum(
 )
 
 
-class Range:
-    def __init__(self, offset: int = 0, length: int = 0):
-        # sanity checks
-        errors = []
-        if offset < 0:
-            errors += ["invalid.must.be.positive.or.zero:offset"]
-        if length < 0:
-            errors += ["invalid.must.be.positive.or.zero:length"]
-        if len(errors) > 0:
-            listBody = ",".join(errors)
-            raise ValueError(*errors)
-        self._offset = offset
-        self._length = length
-
-    @property
-    def offset(self) -> int:
-        return self._offset
-
-    @property
-    def length(self) -> int:
-        return self._length
-
-    @property
-    def end(self) -> int:
-        return self._offset + self._length
-
-    # ========[ predicates ] ========
-    def isEmpty(self) -> bool:
-        return self._length == 0
-
-    def isBefore(self, x: "Range") -> bool:
-        return (x is not None) and self._isBefore(x)
-
-    def isEndingBefore(self, x: "Range") -> bool:
-        return (x is not None) and (self.offset + self.length <= x.offset + x.length)
-
-    def isSmaller(self, x: "Range") -> bool:
-        return x is not None and self._isSmaller(x)
-
-    def isOutside(self, x: "Range") -> bool:
-        return (x is None) or x._isBeginningAfter(self) or self._isBeginningAfter(x)
-
-    def isInside(self, x: "Range") -> bool:
-        return (x is not None) and x._isBefore(self) and self._isEndingBefore(x)
-
-    # ========[ predicates internal ] ========
-    def _isBefore(self, x: "Range") -> bool:
-        return self.offset <= x.offset
-
-    def _isEndingBefore(self, x: "Range") -> bool:
-        return self.offset + self.length <= x.offset + x.length
-
-    def _isBeginningAfter(self, x: "Range") -> bool:
-        return self.offset >= x.offset + x.length
-
-    def _isSmaller(self, x: "Range") -> bool:
-        return self.length <= x.length
-
-
 class Node:
     def __init__(
         self,
