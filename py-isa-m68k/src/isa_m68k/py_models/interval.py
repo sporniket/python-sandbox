3492e9324a98e566c3dd85725c422b675552c861
David SPORN <sporniket.studio@gmail.com>
2024-09-06 08:16:45 +0200

    start refactoring -- write py_models.Interval from static_parser.Range

---
+++ py-isa-m68k/src/isa_m68k/py_models/interval.py
@@ -0,0 +1,105 @@
+"""
+An _interval_ between a and b, with `a <= b`, will mean all the numbers between a _included_ and b _excluded_,
+i.e. in mathematical notation : `[a..b[`
+
+* `a` is the `start` of the interval
+* `b` is the `end` of the interval
+* `b - a` is the length of the interval
+---
+(c) 2024 David SPORN
+---
+This is part of py-isa-m68k -- Toolbox for the Motorola 68k Instruction Set Architecture.
+
+py-isa-m68k is free software: you can redistribute it and/or
+modify it under the terms of the GNU General Public License as published by the
+Free Software Foundation, either version 3 of the License, or (at your option)
+any later version.
+
+py-isa-m68k is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+or FITNESS FOR A PARTICULAR PURPOSE.
+
+See the GNU General Public License for more details.
+You should have received a copy of the GNU General Public License along with py-isa-m68k.
+If not, see <https://www.gnu.org/licenses/>. 
+---
+"""
+
+
+class Interval:
+    def __init__(self, start: int = 0, *, length: int = None, end: int = None):
+        """An interval is defined by its start value and either its end, or its length"""
+        # sanity checks
+        errors = []
+        if start < 0:
+            errors += ["invalid.must.be.positive.or.zero:start"]
+        if length is not None:
+            if end is not None:
+                errors += ["invoke.too.much.argument"]
+                self._checkEnd(start, end, errors)
+            if length < 0:
+                errors += ["invalid.must.be.positive.or.zero:length"]
+        elif end is not None:
+            self._checkEnd(start, end, errors)
+        else:
+            errors += ["invoke.missing.second.argument"]
+
+        if len(errors) > 0:
+            listBody = ",".join(errors)
+            raise ValueError(*errors)
+
+        # OK, proceed
+        self._start = start
+        self._length = length if length is not None else end - start
+        self._end = end if end is not None else start + length
+
+    def _checkEnd(self, start, end, errors):
+        if end < start:
+            errors += ["invalid.too.low:end"]
+
+    @property
+    def start(self) -> int:
+        return self._start
+
+    @property
+    def length(self) -> int:
+        return self._length
+
+    @property
+    def end(self) -> int:
+        return self._end
+
+    def __len__(self):
+        return self.length
+
+    # ========[ predicates ] ========
+    def isEmpty(self) -> bool:
+        return self._length == 0
+
+    def isBefore(self, x: "Interval") -> bool:
+        return (x is not None) and self._isBefore(x)
+
+    def isEndingBefore(self, x: "Interval") -> bool:
+        return (x is not None) and (self.start + self.length <= x.start + x.length)
+
+    def isSmaller(self, x: "Interval") -> bool:
+        return x is not None and self._isSmaller(x)
+
+    def isOutside(self, x: "Interval") -> bool:
+        return (x is None) or x._isBeginningAfter(self) or self._isBeginningAfter(x)
+
+    def isInside(self, x: "Interval") -> bool:
+        return (x is not None) and x._isBefore(self) and self._isEndingBefore(x)
+
+    # ========[ predicates internal ] ========
+    def _isBefore(self, x: "Interval") -> bool:
+        return self.start <= x.start
+
+    def _isEndingBefore(self, x: "Interval") -> bool:
+        return self.start + self.length <= x.start + x.length
+
+    def _isBeginningAfter(self, x: "Interval") -> bool:
+        return self.start >= x.start + x.length
+
+    def _isSmaller(self, x: "Interval") -> bool:
+        return self.length <= x.length
---
+++ py-isa-m68k/tests/behaviour/py_models/test_Interval.py
@@ -0,0 +1,172 @@
+"""
+---
+(c) 2024 David SPORN
+---
+This is part of py-isa-m68k -- Toolbox for the Motorola 68k Instruction Set Architecture.
+
+py-isa-m68k is free software: you can redistribute it and/or
+modify it under the terms of the GNU General Public License as published by the
+Free Software Foundation, either version 3 of the License, or (at your option)
+any later version.
+
+py-isa-m68k is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+or FITNESS FOR A PARTICULAR PURPOSE.
+
+See the GNU General Public License for more details.
+You should have received a copy of the GNU General Public License along with py-isa-m68k.
+If not, see <https://www.gnu.org/licenses/>. 
+---
+"""
+
+import pytest
+from isa_m68k.py_models import Interval
+
+
+def test_Interval_has_expected_properties():
+    # Legal instanciations
+    a, b, c = Interval(length=0), Interval(1, length=5), Interval(1, end=6)
+    assert a.start == 0
+    assert a.length == 0
+    assert a.end == 0
+    assert len(a) == 0
+
+    assert b.start == 1
+    assert b.length == 5
+    assert b.end == 6
+    assert len(b) == 5
+
+    assert c.start == 1
+    assert c.length == 5
+    assert c.end == 6
+    assert len(c) == 5
+
+
+def test_Interval_forbids_illegal_instanciations():
+    # Each type of error
+    with pytest.raises(ValueError) as error:
+        Interval(-1, length=0)
+    assert "invalid.must.be.positive.or.zero:start" in error.value.args
+    assert len(error.value.args) == 1
+
+    with pytest.raises(ValueError) as error:
+        Interval(0, length=-1)
+    assert "invalid.must.be.positive.or.zero:length" in error.value.args
+    assert len(error.value.args) == 1
+
+    with pytest.raises(ValueError) as error:
+        Interval(0, end=-1)
+    assert "invalid.too.low:end" in error.value.args
+    assert len(error.value.args) == 1
+
+    with pytest.raises(ValueError) as error:
+        Interval(0)
+    assert "invoke.missing.second.argument" in error.value.args
+    assert len(error.value.args) == 1
+
+    with pytest.raises(ValueError) as error:
+        Interval(0, length=0, end=0)
+    assert "invoke.too.much.argument" in error.value.args
+    assert len(error.value.args) == 1
+
+    # Will report all the errors
+    with pytest.raises(ValueError) as error:
+        Interval(-1)
+    assert "invalid.must.be.positive.or.zero:start" in error.value.args
+    assert "invoke.missing.second.argument" in error.value.args
+    assert len(error.value.args) == 2
+
+    with pytest.raises(ValueError) as error:
+        Interval(-1, length=-1)
+    assert "invalid.must.be.positive.or.zero:start" in error.value.args
+    assert "invalid.must.be.positive.or.zero:length" in error.value.args
+    assert len(error.value.args) == 2
+
+    with pytest.raises(ValueError) as error:
+        Interval(-1, length=-1, end=-2)
+    assert "invalid.must.be.positive.or.zero:start" in error.value.args
+    assert "invalid.must.be.positive.or.zero:length" in error.value.args
+    assert "invoke.too.much.argument" in error.value.args
+    assert "invalid.too.low:end" in error.value.args
+    assert len(error.value.args) == 4
+
+
+def test_Interval_isEmpty_works_as_expected():
+    (a, b) = (Interval(0, length=0), Interval(0, length=1))
+    assert a.isEmpty()
+    assert not b.isEmpty()
+
+
+def test_Interval_isBefore_is_an_ordering_relation():
+    (a, b, c) = (Interval(9, length=0), Interval(10, length=0), Interval(11, length=0))
+    # isBefore is transitive
+    assert a.isBefore(b)
+    assert b.isBefore(c)
+    assert a.isBefore(c)
+    # isBefore is anti symetrical
+    assert not b.isBefore(a)
+    # isBefore is reflexive
+    assert b.isBefore(b)
+    # is never before null
+    assert not b.isBefore(None)
+
+
+def test_Interval_isSmaller_is_an_ordering_relation():
+    (a, b, c) = (Interval(0, length=9), Interval(0, length=10), Interval(0, length=11))
+    # isSmaller is transitive
+    assert a.isSmaller(b)
+    assert b.isSmaller(c)
+    assert a.isSmaller(c)
+    # isSmaller is anti symetrical
+    assert not b.isSmaller(a)
+    # isSmaller is reflexive
+    assert b.isSmaller(b)
+    # is not smaller than None
+    assert not b.isSmaller(None)
+
+
+def test_Interval_isEndingBefore_is_an_ordering_relation():
+    (a, b, c) = (
+        Interval(9, length=10),
+        Interval(10, length=10),
+        Interval(11, length=10),
+    )
+    # isEndingBefore is transitive
+    assert a.isEndingBefore(b)
+    assert b.isEndingBefore(c)
+    assert a.isEndingBefore(c)
+    # isEndingBefore is anti symetrical
+    assert not b.isEndingBefore(a)
+    # isEndingBefore is reflexive
+    assert b.isEndingBefore(b)
+    # is not ending before None
+    assert not b.isEndingBefore(None)
+
+
+def test_Interval_isOutside_works_as_expected():
+    a, b, c = Interval(5, length=10), Interval(15, length=10), Interval(3, length=15)
+    assert a.isOutside(b)
+    assert not a.isOutside(c)
+    assert b.isOutside(a)
+    assert not b.isOutside(c)
+    assert not c.isOutside(a)
+    assert not c.isOutside(b)
+
+
+def test_Interval_isInside_is_an_ordering_relation():
+    (a, b, c) = (Interval(9, length=2), Interval(8, length=4), Interval(7, length=6))
+    # isInside is transitive
+    assert a.isInside(b)
+    assert b.isInside(c)
+    assert a.isInside(c)
+    # isInside is anti symetrical
+    assert not b.isInside(a)
+    # isInside is reflexive
+    assert b.isInside(b)
+    # is not inside None
+    assert not b.isInside(None)
+    # other things
+    assert not a.isInside(Interval(7, length=2))
+    assert not a.isInside(Interval(8, length=2))
+    assert not a.isInside(Interval(10, length=2))
+    assert not a.isInside(Interval(11, length=2))
