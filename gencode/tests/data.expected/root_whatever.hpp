7136d497344d3c419bfbe3ed47c9ddedc95415fe
David SPORN <sporniket.studio@gmail.com>
2024-09-30 07:49:39 +0200

    gencode blank : change expected header file

---
+++ gencode/tests/data.expected/root_whatever.hpp
@@ -0,0 +1,9 @@
+// no licence
+#ifndef WHATEVER_HPP
+#define WHATEVER_HPP
+// ================[ CODE BEGINS ]================
+
+// ...your code...
+
+// ================[ END OF CODE ]================
+#endif
