ae0cc98116ec37e853ee646108443c2d6cf72260
David SPORN <sporniket.studio@gmail.com>
2024-11-09 19:20:41 +0100

    prjtabouret -- generate main keys, fill globals, switches

---
+++ prjtabouret/gendatabase.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python3
+
+import json
+import sys
+
+def initStructRoot(x):
+    for n in ["GAL16V8_Registered","GAL16V8_Complex","GAL16V8_Simple"]:
+        x[n] = {}
+        for s in ["pins","specials","blocks", "macrocells","switches","globals"]:
+            x[n][s] = None
+
+def initGlobals(x, modeName, modeValue):
+    x["globals"] = {
+        "mode":{ # AC1 fuse
+            "fuses":[2192, 2193], # SYN, AC0
+            "values":{
+                #"registered":2,
+                f"{modeName}":modeValue,
+                #"simple":1
+            }
+        }
+    }
+
+def initSwitches(x):
+    def makeUimBody(i):
+        offset = 32 * i
+        return {
+            "block": "A",
+            "mux": {
+                "fuses": [f+offset for f in range(0,32)]
+            }
+        }
+    x["switches"] = {f"UIM{i}":makeUimBody(i) for i in range(0,64)}
+
+if __name__ == '__main__':
+    targetFileName="database"
+    print(f"generate {targetFileName}.json ...")
+    print()
+
+    # 1.
+    result = {}
+    initStructRoot(result)    
+
+    # 2.
+    for specs in [["GAL16V8_Registered","registered",2],["GAL16V8_Complex","complex",3],["GAL16V8_Simple","simple",1]]:
+        initGlobals(result[specs[0]],specs[1],specs[2])
+
+    # 3.
+    for specs in [["GAL16V8_Registered"],["GAL16V8_Complex"],["GAL16V8_Simple"]]:
+        initSwitches(result[specs[0]])
+
+    # save to file
+    with open(f"./{targetFileName}.json", encoding="utf-8", mode="w") as f:
+        json.dump(result, f, indent=4)
+
+    # end.
+    print("DONE.")
