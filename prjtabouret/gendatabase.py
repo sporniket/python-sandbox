02c4df65111e1e7b1bdf140c77aef45ecdc9e92c
David SPORN <sporniket.studio@gmail.com>
2024-11-10 11:30:06 +0100

    prjtabouret -- generate 'specials', 'macrocells' and 'switches'

--- prjtabouret/gendatabase.py
+++ prjtabouret/gendatabase.py
@@ -32,6 +32,43 @@ def initSwitches(x):
         }
     x["switches"] = {f"UIM{i}":makeUimBody(i) for i in range(0,64)}
 
+def initMacroCells(x):
+    x["macrocells"] = {}
+    for i in range(0,8):
+        ptermsOffset = 256 * i
+        ptdOffset=2128 + 8*i
+
+        x["macrocells"][f"MC{i+1}"] = {
+            "block":"A",
+            "pad":f"M{i+1}",
+            "pterm_ranges":{f"PT{j+1}":[ptermsOffset+32*j,ptermsOffset+32*(j+1)] for j in range(0,8)},
+            "pterm_disables":{f"PTD{j+1}":ptdOffset+j for j in range(0,8)},
+            "configuration":{
+                "fuses":[2120+i],
+                "values":{
+                    "registered":0,
+                    "combinatorial":1
+                }
+            },
+            "polarity":{
+                "fuses":[2048+i],
+                "values":{
+                    "active low":0,
+                    "active high":1
+                }
+            }
+        }
+
+def initSpecials(x):
+    x["GAL16V8_Registered"]["specials"]={
+      "CLK":"C1",
+      "OE":"E1"
+    }
+    x["GAL16V8_Complex"]["specials"]={
+    }
+    x["GAL16V8_Simple"]["specials"]={
+    }
+
 if __name__ == '__main__':
     targetFileName="database"
     print(f"generate {targetFileName}.json ...")
@@ -48,6 +85,13 @@ if __name__ == '__main__':
     # 3.
     for specs in [["GAL16V8_Registered"],["GAL16V8_Complex"],["GAL16V8_Simple"]]:
         initSwitches(result[specs[0]])
+
+    # 4.
+    for specs in [["GAL16V8_Registered"],["GAL16V8_Complex"],["GAL16V8_Simple"]]:
+        initMacroCells(result[specs[0]])
+
+    # 5
+    initSpecials(result)
 
     # save to file
     with open(f"./{targetFileName}.json", encoding="utf-8", mode="w") as f:
